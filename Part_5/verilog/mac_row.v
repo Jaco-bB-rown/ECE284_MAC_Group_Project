b6c8626d70626d2c0de421ae65f2074bd904e0c3
kunalb123 <bhandarkar.kunal@gmail.com>
2024-12-03 22:07:23 -0800

    updating mac_row to handle mode select

--- Part_5/verilog/mac_row.v
+++ Part_5/verilog/mac_row.v
@@ -1,6 +1,6 @@
 // Created by prof. Mingu Kang @VVIP Lab in UCSD ECE department
 // Please do not spread this code without permission 
-module mac_row (clk, out_s, in_w, in_n, valid, inst_w, reset);
+module mac_row (clk, out_s, in_w, in_n, valid, inst_w, reset, mode_select);
 
   parameter bw = 4;
   parameter psum_bw = 16;
@@ -13,6 +13,7 @@ module mac_row (clk, out_s, in_w, in_n, valid, inst_w, reset);
   input  [bw-1:0] in_w; // inst[1]:execute, inst[0]: kernel loading
   input  [1:0] inst_w;
   input  [psum_bw*col-1:0] in_n;
+  input  mode_select;
 
   wire  [(col+1)*bw-1:0] temp;
   wire  [(col+1)*inst_bw-1:0] inst_temp;
@@ -30,7 +31,8 @@ module mac_row (clk, out_s, in_w, in_n, valid, inst_w, reset);
 	       .inst_w(inst_temp[i*inst_bw-1:(i-1)*(inst_bw)]),
 	       .inst_e(inst_temp[(i+1)*inst_bw-1:(i)*(inst_bw)]),
 	       .in_n(in_n[i*psum_bw-1:(i-1)*(psum_bw)]),
-	       .out_s(out_s[(i)*psum_bw-1:(i-1)*(psum_bw)]));
+	       .out_s(out_s[(i)*psum_bw-1:(i-1)*(psum_bw)])
+         .mode_select(mode_select));
          //set valid as inst_e[1] for each tile
          assign valid[i-1] = inst_temp[((i+1)*inst_bw-1)];
   end
