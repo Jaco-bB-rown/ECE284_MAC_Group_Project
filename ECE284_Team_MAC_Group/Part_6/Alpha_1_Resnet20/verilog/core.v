d8d462fcc353d6e5e860021493c62b7a2a1d7956
Jacob Brown <102562306+Jaco-bB-rown@users.noreply.github.com>
2024-12-14 22:33:15 -0800

    done

---
+++ ECE284_Team_MAC_Group/Part_6/Alpha_1_Resnet20/verilog/core.v
@@ -0,0 +1,73 @@
+module core #(
+    parameter bw = 4,           //  act/weight Bit width
+    parameter psum_bw = 16,     // PSUM Bit width
+    parameter row = 8,          // Number of rows
+    parameter col = 8          // Number of columns
+)(
+    input wire clk,
+    input wire reset,
+    input wire [row*bw-1:0] D_xmem,
+    input wire [33:0] inst,
+    output wire [col*psum_bw-1:0] sfp_out,
+    output wire  ofifo_valid
+);
+
+    // SRAM connections for storing activations and weights
+    wire [row*bw-1:0] xmem_out;
+    wire CEN_xmem, CEN_pmem;
+    wire WEN_xmem, WEN_pmem;
+    wire [10:0] xmem_addr, pmem_addr;
+
+    wire [row*bw-1:0] act_in,weight_in; // weight_in not used for weight stationary// dummy variable rn
+    wire [col*psum_bw-1:0] corelet_out, pmem_in, pmem_out, sfp_in;
+
+    assign xmem_addr = inst[17:7];
+    assign pmem_addr = inst[30:20];
+    assign CEN_xmem = inst[19];
+    assign WEN_xmem = inst[18];
+    assign CEN_pmem = inst[32];
+    assign WEN_pmem = inst[31];
+
+    assign act_in = xmem_out;
+    //assign corelet_in = xmem_out;
+    assign pmem_in =  corelet_out;  // if accumulation sfp goes to pmem else corelet goes to pmem
+    assign sfp_out = inst[33] ? corelet_out : 0;;
+    assign ofifo_valid = o_valid;
+    assign sfp_in = inst[33] ? pmem_out : 0;
+
+    // SRAM for activations and weights
+    sram #(.bw(row*bw)) xmem_sram (
+        .CLK(clk),
+        .D(D_xmem),
+        .Q(xmem_out),
+        .CEN(CEN_xmem),
+        .WEN(WEN_xmem),
+        .A(xmem_addr)
+    );
+
+    // SRAM for outputs
+    sram #(.bw(col*psum_bw)) pmem_sram (
+        .CLK(clk),
+        .D(pmem_in),
+        .Q(pmem_out),
+        .CEN(CEN_pmem),
+        .WEN(WEN_pmem),
+        .A(pmem_addr)
+    );
+
+    // Instantiate corelet
+    corelet #(.bw(bw), .psum_bw(psum_bw), .col(col), .row(row)) corelet_inst (
+        .clk(clk),
+        .reset(reset),
+        .inst({inst[33],inst[6:0]}),
+        .activation_in(act_in),
+        .weight_in(weight_in),
+        .sfp_in(sfp_in),
+        .corelet_out(corelet_out),
+        .o_valid(o_valid)
+    );
+
+
+
+
+endmodule
---
+++ ECE284_Team_MAC_Group/Part_6/Alpha_1_Resnet20/verilog/mac_row.v
@@ -0,0 +1,39 @@
+// Created by prof. Mingu Kang @VVIP Lab in UCSD ECE department
+// Please do not spread this code without permission 
+module mac_row (clk, out_s, in_w, in_n, valid, inst_w, reset);
+
+  parameter bw = 4;
+  parameter psum_bw = 16;
+  parameter col = 8;
+  parameter inst_bw = 2;
+
+  input  clk, reset;
+  output [psum_bw*col-1:0] out_s;
+  output [col-1:0] valid;
+  input  [bw-1:0] in_w; // inst[1]:execute, inst[0]: kernel loading
+  input  [1:0] inst_w;
+  input  [psum_bw*col-1:0] in_n;
+
+  wire  [(col+1)*bw-1:0] temp;
+  wire  [(col+1)*inst_bw-1:0] inst_temp;
+
+  assign temp[bw-1:0]   = in_w;
+  assign inst_temp[inst_bw-1:0] = inst_w;
+
+  genvar i;
+  generate for (i=1; i < col+1 ; i=i+1) begin : col_num
+      mac_tile #(.bw(bw), .psum_bw(psum_bw)) mac_tile_instance (
+         .clk(clk),
+         .reset(reset),
+	       .in_w( temp[bw*i-1:bw*(i-1)]),
+	       .out_e(temp[bw*(i+1)-1:bw*i]),
+	       .inst_w(inst_temp[i*inst_bw-1:(i-1)*(inst_bw)]),
+	       .inst_e(inst_temp[(i+1)*inst_bw-1:(i)*(inst_bw)]),
+	       .in_n(in_n[i*psum_bw-1:(i-1)*(psum_bw)]),
+	       .out_s(out_s[(i)*psum_bw-1:(i-1)*(psum_bw)])
+         );
+         //set valid as inst_e[1] for each tile
+         assign valid[i-1] = inst_temp[((i+1)*inst_bw-1)];
+  end
+  endgenerate
+endmodule
